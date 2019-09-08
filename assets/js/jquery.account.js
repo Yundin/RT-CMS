720c4c85fde8b817c4a5a7968960cb16f2d8d87d
Vladislav Yundin <4yundin@gmail.com>
2019-09-09 00:28:46 +0300

    Chat window added

--- assets/js/jquery.account.js
+++ assets/js/jquery.account.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	var iconsCount = 0
 	var desktop = $("#desktop")
 	var dock = $("#dock")
-	if (thereIsProfile) {
+	if (isThereProfile) {
 		var pos = countMargins(iconsCount++)
 		var a = `<a class="abs icon" style="left:${pos[0]}px;top:${pos[1]}px;" href="#icon_dock_profile"> <img src="assets/images/icons/icon_32_network.png" />Profile</a>`
 		desktop.append(a)
@@ -10,6 +10,16 @@ $(document).ready(function() {
 			desktop.append(data)
 		})
 		var li = '<li id="icon_dock_profile"><a href="#window_profile"><img src="assets/images/icons/icon_22_network.png" />Profile</a></li>'
+		dock.append(li)
+	}
+	if (isThereChat) {
+		var pos = countMargins(iconsCount++)
+		var a = `<a class="abs icon" style="left:${pos[0]}px;top:${pos[1]}px;" href="#icon_dock_chat"> <img src="assets/images/icons/icon_32_network.png" />Chat</a>`
+		desktop.append(a)
+		$.get("assets/pages/chat.html", function(data) {
+			desktop.append(data)
+		})
+		var li = '<li id="icon_dock_chat"><a href="#window_chat"><img src="assets/images/icons/icon_22_network.png" />Chat</a></li>'
 		dock.append(li)
 	}
 });
